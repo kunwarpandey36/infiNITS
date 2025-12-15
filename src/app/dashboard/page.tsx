31aa3137aad0432ada0b456e947a6352307e2fd9
kunwarpandey7 <kunwarpandey7@gmail.com>
2025-12-15 10:53:03 +0000

    commit on the 15th dec 2025

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,7 +21,7 @@ export default function DashboardPage() {
     <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
       <div className="flex items-center justify-between space-y-2 mb-4">
         <h2 className="text-3xl font-bold tracking-tight font-headline">
-          Welcome, <span className="text-primary">{student?.name || 'Student'}</span>!
+          swagatam, <span className="text-primary">{student?.name || 'Student'}</span>!
         </h2>
       </div>
       <div className="space-y-6">
